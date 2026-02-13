56d5d84593c599b19f417844b5906c7095ef2432
Juan Pablo Rolo <juanpablorolo2007@gmail.com>
2026-02-13 05:21:48 +0000

    a

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,9 @@
 require("dotenv").config();
 
 const app = require("./app");
-const { connectDb } = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 
-connectDb()
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Servidor iniciado correctamente en el puerto ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error al iniciar el servidor:", error);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+  console.log("Servidor iniciado SIN conectar DB en puerto " + PORT);
+});
